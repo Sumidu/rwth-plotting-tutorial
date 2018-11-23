0205a90090f16359f12088bf7573a59236ce6536
anneiou <anne.vollema@rwth-aachen.de>
2018-11-23 12:11:04 +0100

    Boxplot für Geschlecht und Größe

--- FreyschmidtVollema.R
+++ FreyschmidtVollema.R
@@ -21,12 +21,31 @@ data()
 
 # Auswahl Datensatz
 data(starwars)
-starwarslibrary(ggplot2)
+starwars
+library(ggplot2)
 
 # Test Boxplot Größe "height"
 ggplot(data = starwars) +
   aes(x = height, y = height) +
   geom_boxplot(fill = "#ffffff") +
   coord_flip() +
+  theme_minimal()
+library(ggplot2)
+
+ggplot(data = starwars) +
+  aes(x = eye_color, y = skin_color) +
+  geom_tile() +
+  labs(title = "Erscheinungsbild Universumsbewohner",
+    x = "Augenfarbe",
+    y = "Hautfarbe",
+    subtitle = "Vergleich Hautfarbe mit Augenfarbe") +
+  theme_minimal()
+ggplot(data = starwars) +
+  aes(x = gender, y = height) +
+  geom_boxplot(fill = "#0c4c8a") +
+  labs(title = "Erscheinungsbild Universumsbewohner",
+    x = "Geschlecht",
+    y = "Größe",
+    subtitle = "Vergleich Geschlecht und Größe") +
   theme_minimal()
 
