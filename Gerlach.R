3b34d35dbcd2a95cd0ba71b8c03624609d72fd3b
Tom Gerlach <frank-gerlach@139-115.eduroam.rwth-aachen.de>
2018-11-28 14:14:32 +0100

    Erstellung Balkendiagramm

--- Gerlach.R
+++ Gerlach.R
@@ -5,6 +5,20 @@ library(psych)
 data(starwars)
 starwars
 
+library(ggplot2)
+
+ggplot(data = starwars) +
+  aes(x = gender, weight = mass) +
+  geom_bar(fill = '#e5f5e0') +
+  labs(title = 'Männliche Wesen aus StarWars sind am schwersten',
+    x = 'Geschlecht',
+    y = 'Masse',
+    subtitle = 'Balkendiagramm Masse nach Geschlecht') +
+  theme_minimal()
+
+ggsave("Gerlach_bar.pdf")
+
+
 
 
 
