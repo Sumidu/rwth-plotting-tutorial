41cba696ee7e55d200d4d1fe67602b956efd00db
Julian Hildebrandt <julianhildebrandt@130-226.eduroam.rwth-aachen.de>
2018-12-07 10:03:01 +0100

    rwth farben

---
+++ corporateIdentity.R
@@ -0,0 +1,23 @@
+
+#' rwth.colorplette
+#'
+#' @return List of RWTH Colors (2018)
+#' @export
+#'
+rwth.colorpalette <- function(){
+  list(blue = rgb2hex(0,84,159),
+       lightblue = rgb2hex(142,186,229),
+       black= rgb2hex(0,0,0),
+       magenta = rgb2hex(227,0,102),
+       yellow = rgb2hex(255,237,0),
+       petrol = rgb2hex(0,97,101),
+       turquois = rgb2hex(0,152,161),
+       green = rgb2hex(87,171,39),
+       maygreen = rgb2hex(189,205,0),
+       orange = rgb2hex(246,168,0),
+       red = rgb2hex(204,7,30),
+       bordeaux = rgb2hex(161,16,53),
+       violet = rgb2hex(97,33,88),
+       purple = rgb2hex(122,111,172)
+  )
+}
