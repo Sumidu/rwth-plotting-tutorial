596f55792e1b1e1ad371d3ea6cf9788e334f464a
Leonie Düttmann <leonie@162-060.eduroam.rwth-aachen.de>
2018-12-07 11:40:44 +0100

    Push für Laura

---
+++ DuettmannSchroederInferenzstatistik.R
@@ -0,0 +1,9 @@
+#### Inferenzstatistik
+
+source("install_libraries.R")
+source("coporateIdentity.R")
+
+rwthcolor <- hcictools :: rwth.colorpalette()
+
+data_robot <- readRDS("data/robo_pflege.rds")
+
