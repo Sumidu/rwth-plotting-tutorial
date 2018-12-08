69659eedd37f83fb8bd89f0000ddc0c9166f589b
ablancle <anna.leblanc@rwth-aachen.de>
2018-12-08 14:04:31 +0100

    weg comitten

---
+++ LeblancNiessenInferenzstatistik.R
@@ -0,0 +1,19 @@
+### Inferenzstatistik 
+
+
+source("install_libraries.R")
+
+source("corporateIdentity.R")
+
+
+rwthcolor <- hcictools::rwth.colorpalette()
+
+library(tidyverse)
+library(ggthemes)
+library(plotrix)
+library(jmv)
+
+data_robot <- readRDS("data/robo_pflege.rds")
+
+t.test()
+
