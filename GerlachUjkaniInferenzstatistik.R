8c23d147bea816015bafe203c23ba6d1f2fde6be
Tom Gerlach <frank-gerlach@151-207.eduroam.rwth-aachen.de>
2018-12-11 13:39:27 +0100

    Anpassung Grafikbreite

--- GerlachUjkaniInferenzstatistik.R
+++ GerlachUjkaniInferenzstatistik.R
@@ -46,4 +46,4 @@ data_robot %>%
        caption = "Fehlerbalken zeigen Standardfehler des Mittelwertes") +
   NULL
 
-ggsave(file="GerlachUjkaniPunktdiagramm.pdf", width=10)
+ggsave(file="GerlachUjkaniPunktdiagramm.pdf", width=11)
