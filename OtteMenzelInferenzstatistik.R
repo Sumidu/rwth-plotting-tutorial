83e9f5cf5d34963ce9ebb1d7da130aef34a27048
alexaotte <alexa.otte@rwth-aachen.de>
2018-12-11 15:05:30 +0100

    Otte Menzel Inferenzstatistik bearbeitet (tbc)

---
+++ OtteMenzelInferenzstatistik.R
@@ -0,0 +1,40 @@
+### Alexa Otte und Insa Menzel Inferenzstatistik
+
+source("install_libraries.R")
+library(tidyverse)
+library(tidyverse)
+library(ggthemes)
+library(ggplot2)
+library(plotrix)
+library(jmv)
+
+## Farben verfügbar machen:
+rwthcolor <- hcictools::rwth.colorpalette()
+
+## Daten Laden
+data_robot <- readRDS("Data/robo_pflege.rds")
+
+## Unverbundener T-Test. Hypothese: Männer und Frauen unterscheiden sich im Hinblick auf robo_bed.
+t.test( filter(data_robot, gender == "weiblich")$robo_bed, 
+        filter(data_robot, gender == "männlich")$robo_bed )
+
+
+data_robot %>% 
+  filter(gender != "keine Angabe") %>% 
+  group_by(gender) %>% 
+  summarise(mean_robo_bed = mean(robo_bed)-1, sem_robo_be = std.error(robo_bed)) %>% 
+  ggplot() +
+  aes(x = gender, weight = mean_robo_bed, fill = gender, ymin = mean_robo_bed-sem_robo_bed, ymax = mean_robo_bed+sem_robo_bed) +
+  geom_bar(width = 0.5) +
+  scale_fill_manual(values = c(rwthcolor$blue, rwthcolor$red)) +
+  geom_errorbar(width = 0.2) +
+  theme_minimal() +
+  ylim(0,5) +
+  labs(title = "blabbla", 
+       subtitle = "Balkendiagramm: robo_bed im Vergleich zwischen Männern und Frauen ", 
+       x = "Geschlecht",
+       y = "robo_bed [0 - 5]",
+       fill = "Geschlecht",
+       caption = "Fehlerbalken zeigen Standardfehler des Mittelwertes.") +
+  NULL
+# ggsave("OtteMenzelBalkendiagrammTtest.pdf", width = 6, height = 5)
