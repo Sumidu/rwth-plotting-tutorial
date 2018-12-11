dee06c67dbab7eb51f82a396d4cabf39b4f53208
Tom Gerlach <frank-gerlach@151-207.eduroam.rwth-aachen.de>
2018-12-11 13:18:33 +0100

    Erstellung R Skript

---
+++ GerlachUjkaniInferenzstatistik.R
@@ -0,0 +1,49 @@
+### Inferenzstatistik Gerlach & Ujkani
+
+
+# source("install_libraries.R")
+library(tidyverse)
+library(tidyverse)
+library(ggthemes)
+library(ggplot2)
+library(plotrix)
+library(jmv)
+
+## Farben verfügbar machen:
+rwthcolor <- hcictools::rwth.colorpalette()
+
+## Daten Laden
+data_robot <- readRDS("data/robo_pflege.rds")
+
+## Zweifaktorielle ANOVA. Hypothese: Geschlecht und Jobart haben einen Einfluss auf DIFFPREF.
+
+data_robot %>%
+  filter(gender != "keine Angabe") %>%
+  filter(job_type == "Arbeitnehmer/in" | job_type == "Studierend/Schüler/in" ) %>%
+  jmv::anova(dep = "diffpref",
+             factors = c("job_type", "gender")
+  ) -> res
+
+res$main
+
+data_robot %>%
+  filter(gender != "keine Angabe") %>%
+  filter(job_type == "Arbeitnehmer/in" | job_type == "Studierend/Schüler/in" ) %>%
+  group_by(gender, job_type) %>%
+  summarise(mean_diffpref = mean(diffpref), sem_diffpref = std.error(diffpref)) %>%
+  ggplot() +
+  aes(x = job_type, y = mean_diffpref, colour = gender, ymin = mean_diffpref-sem_diffpref, ymax = mean_diffpref+ sem_diffpref) +
+  geom_errorbar(width = 0.2, colour = rwthcolor$black) +
+  geom_point(size = 3) +
+  geom_line(aes(group = gender)) +
+  scale_colour_manual(values = c(rwthcolor$blue, rwthcolor$red)) +
+  theme_grey() +
+  labs(title = "Männer, Schüler und Studierende haben eine höhere Schwierigkeitspräferenz!",
+       subtitle = "Punktdiagramm: DIFFPREF im Vergleich zwischen ArbeitnehmerInnen und Studierenden, gruppiert nach Geschlecht",
+       x = "Beruf",
+       y = "DIFFPREF [1 - 6]",
+       colour = "Geschlecht",
+       caption = "Fehlerbalken zeigen Standardfehler des Mittelwertes") +
+  NULL
+
+#ggsave("GerlachUjkaniPunktdiagramm.pdf")
