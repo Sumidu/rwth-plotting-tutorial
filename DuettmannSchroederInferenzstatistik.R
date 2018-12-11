3175ee449ae6ad8a76bf18b8a67c175f5032cbd7
lauraschr <laura.schroeder1@rwth-aachen.de>
2018-12-11 17:36:24 +0100

    R hasst mich heute

--- DuettmannSchroederInferenzstatistik.R
+++ DuettmannSchroederInferenzstatistik.R
@@ -1,9 +1,45 @@
-#### Inferenzstatistik
+#### Inferenzstatistik Leonie und Laura
+
 
 source("install_libraries.R")
-source("coporateIdentity.R")
+library(tidyverse)
+library(tidyverse)
+library(ggthemes)
+library(ggplot2)
+library(plotrix)
+library(jmv)
 
-rwthcolor <- hcictools :: rwth.colorpalette()
+## Farben verfügbar machen:
+rwthcolor <- hcictools::rwth.colorpalette()
 
-data_robot <- readRDS("data/robo_pflege.rds")
+## Daten Laden
+data_robot <- readRDS("Data/robo_pflege.rds")
+
+
+# unverbundener T-Test. Hypothese: Männer und Frauen unterscheiden sich bei robo_bath.
+
+t.test (filter (data_robot, gender == "weiblich")$robo_bath,
+        filter(data_robot, gender =="männlich")$robo_bath)
+
+data_robot %>% 
+  filter(gender != "keine Angabe") %>% 
+  group_by(gender) %>% 
+  summarise(mean_robo_bath = mean(robo_bath)-1, sem_robo_bath = std.error(robo_bath)) %>% 
+  ggplot() +
+  aes(x = gender, weight = mean_robo_bath, fill = gender, ymin = mean_robo_bath - sem_robo_bath, ymax = mean_robo_bath + sem_robo_bath) +
+  geom_bar(width = 0.5) +
+  scale_fill_manual(values = c(rwthcolor$blue, rwthcolor$red)) +
+  geom_errorbar(width = 0.2) +
+  theme_minimal() +
+  ylim(0,5) +
+  labs(title = "Männer lassen sich eher von einem Roboter baden als Frauen", 
+       subtitle = "Balkendiagramm: robo_bath im Vergleich zwischen Männern und Frauen ", 
+       x = "Geschlecht",
+       y = "robo_bath [0 - 5]",
+       fill = "Geschlecht",
+       caption = "Fehlerbalken zeigen Standardfehler des Mittelwertes.") +
+  NULL
+
+
+
 
