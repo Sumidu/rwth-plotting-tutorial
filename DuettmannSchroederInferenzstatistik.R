0130349ba514d7c3ea7b7c096b67ef87ced5eeda
lauraschr <laura.schroeder1@rwth-aachen.de>
2018-12-12 14:49:18 +0100

    Fertiges Balkendiagramm

--- DuettmannSchroederInferenzstatistik.R
+++ DuettmannSchroederInferenzstatistik.R
@@ -21,25 +21,24 @@ data_robot <- readRDS("Data/robo_pflege.rds")
 t.test (filter (data_robot, gender == "weiblich")$robo_bath,
         filter(data_robot, gender =="männlich")$robo_bath)
 
+
+
 data_robot %>% 
   filter(gender != "keine Angabe") %>% 
   group_by(gender) %>% 
   summarise(mean_robo_bath = mean(robo_bath)-1, sem_robo_bath = std.error(robo_bath)) %>% 
-  ggplot() +
-  aes(x = gender, weight = mean_robo_bath, fill = gender, ymin = mean_robo_bath - sem_robo_bath, ymax = mean_robo_bath + sem_robo_bath) +
-  geom_bar(width = 0.5) +
+ggplot() +
+  aes(x = gender, weight = robo_bath, fill = gender, ymin = mean_robo_bath - sem_robo_bath, ymax = mean_robo_bath + sem_robo_bath) +
   scale_fill_manual(values = c(rwthcolor$blue, rwthcolor$red)) +
+  geom_bar(width = 0.5) +
   geom_errorbar(width = 0.2) +
-  theme_minimal() +
   ylim(0,5) +
-  labs(title = "Männer lassen sich eher von einem Roboter baden als Frauen", 
-       subtitle = "Balkendiagramm: robo_bath im Vergleich zwischen Männern und Frauen ", 
-       x = "Geschlecht",
-       y = "robo_bath [0 - 5]",
-       fill = "Geschlecht",
-       caption = "Fehlerbalken zeigen Standardfehler des Mittelwertes.") +
-  NULL
-
-
+  theme_gray() +
+  labs(title = "Frauen lassen sich eher von einem Roboter baden als Männer", 
+     subtitle = "Balkendiagramm: robo_bath im Vergleich zwischen Männern und Frauen ", 
+     x = "Geschlecht",
+     y = "robo_bath [0 - 5]",
+     fill = "Geschlecht",
+     caption = "Fehlerbalken zeigen Standardfehler des Mittelwertes.") 
 
 
