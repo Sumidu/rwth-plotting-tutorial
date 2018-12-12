a64311b45d4bfd0867b547ce65ce67376f6b7d2d
frepai <freya.paintner@rwth-aachen.de>
2018-12-12 13:34:38 +0100

    Tutorial durchgeführt und PDF generiert

--- HerboltPaintner_Inferenzstatistik.R
+++ HerboltPaintner_Inferenzstatistik.R
@@ -1,96 +1,75 @@
 ###Inferenzstatistik Herbolt, Paintner
 
-source("install_libraries.R")
+#source("install_libraries.R")
 
 source("corporateIdentity.R")
 
+# source("install_libraries.R")
+library(tidyverse)
 library(tidyverse)
 library(ggthemes)
-library(plitrix)
+library(ggplot2)
+library(plotrix)
 library(jmv)
 
 rwthcolor <- hcictools::rwth.colorpalette()
 
 data_robot <- readRDS("data/robo_pflege.rds")
 
-## Unverbundener T-Test. Hypothese: Männer und Frauen unterscheiden sich im KUT.
-t.test( filter(data_robot, gender == "weiblich")$kut, 
-        filter(data_robot, gender == "männlich")$kut )
+## Unverbundener T-Test. Hypothese: Männer und Frauen unterscheiden sich im Technikverständnis.
+t.test( filter(data_robot, gender == "weiblich")$tv, 
+        filter(data_robot, gender == "männlich")$tv )
 
-
-
-data_robot %>% 
-  filter(gender != "keine Angabe") %>% 
-  group_by(gender) %>% 
-  summarise(mean_kut = mean(kut)-1, sem_kut = std.error(kut)) %>% 
-  ggplot() +
-  aes(x = gender, weight = mean_kut, fill = gender, ymin = mean_kut-sem_kut, ymax = mean_kut+sem_kut) +
-  geom_bar(width = 0.5) +
-  scale_fill_manual(values = c(rwthcolor$blue, rwthcolor$red)) +
-  geom_errorbar(width = 0.2) +
-  theme_minimal() +
-  ylim(0,5) +
-  labs(title = "Männer glauben eher daran, Technik kontrollieren zu können", 
-       subtitle = "Balkendiagramm: KUT im Vergleich zwischen Männern und Frauen ", 
-       x = "Geschlecht",
-       y = "KUT [0 - 5]",
-       fill = "Geschlecht",
-       caption = "Fehlerbalken zeigen Standardfehler des Mittelwertes.") +
+data_robot %>%
+  filter(gender != "keine Angabe") %>%
+  group_by(gender) %>%
+  summarise(mean_tv = mean(tv) -1, sem_tv = std.error(tv)) %>%
+ggplot() +
+  aes(x = gender, weight = mean_tv, fill = gender, ymin = mean_tv - sem_tv, ymax = mean_tv + sem_tv) +
+  scale_fill_manual(values = c(rwthcolor$petrol, rwthcolor$magenta)) +
+  geom_bar( width = 0.5) +
+  geom_errorbar( width = 0.2) +
+  ylim(0,4) +
+  theme_gray() +
+  labs(title = "Männer glauben, ein höheres Technikverständnis als Frauen zu haben.", 
+     subtitle = "Balkendiagramm: TV im Vergleich zwischen Männern und Frauen ", 
+     x = "Geschlecht",
+     y = "TV [0 - 4]",
+     fill = "Geschlecht",
+     caption = "Fehlerbalken zeigen Standardfehler des Mittelwertes.") +
   NULL
 
-ggsave("HerboltPaintnerBalkendiagrammTtest.pdf", width = 6, height = 5)
 
 
-data_robot %>% 
-  filter(gender != "keine Angabe") %>% 
-  group_by(gender) %>% 
-  summarise(mean_kut = mean(kut), sem_kut = std.error(kut)) %>% 
-  ggplot() +
-  aes(x = gender, y = mean_kut, colour = gender, ymin = mean_kut-sem_kut, ymax = mean_kut+sem_kut) +
-  geom_errorbar(width = 0.2, colour = rwthcolor$black) +
-  geom_point(size = 3) +
-  scale_colour_manual(values = c(rwthcolor$blue, rwthcolor$red)) +
-  theme_minimal() +
-  ylim(3.5,5) +
-  labs(title = "Männer glauben eher daran, Technik kontrollieren zu können", 
-       subtitle = "Punktdiagramm: KUT im Vergleich zwischen Männern und Frauen ", 
-       x = "Geschlecht",
-       y = "KUT [1 - 6]",
-       colour = "Geschlecht",
-       caption = "Fehlerbalken zeigen Standardfehler des Mittelwertes. Y-Achse ist aus Gründen der Lesbarkeit verschoben.") +
-  NULL
-#ggsave("HerboltPaintnerPunktdiagrammTtest.pdf", width = 6, height = 5)
 
-
-## Zweifaktorielle ANOVA. Hypothese: Geschlecht und Erwerbstätigenstatus haben einen Einfluss auf KUT.
+## Zweifaktorielle ANOVA. Hypothese: Geschlecht und Erwerbstätigenstatus haben einen Einfluss auf das Technikverständnis.
 data_robot %>% 
   filter(gender != "keine Angabe") %>%
   filter(job_type == "Arbeitnehmer/in" | job_type == "Studierend/Schüler/in" ) %>% 
-  jmv::anova(dep = "kut", 
+  jmv::anova(dep = "tv", 
              factors = c("job_type", "gender") 
   ) -> res
-
 
 res$main 
 
 data_robot %>% 
   filter(gender != "keine Angabe") %>%
-  filter(job_type == "Arbeitnehmer/in" | job_type == "Studierend/Schüler/in" ) %>% 
+  filter(job_type == "Arbeitnehmer/in" | job_type == "Rentner/in" ) %>% 
   group_by(gender, job_type) %>% 
-  summarise(mean_kut = mean(kut), sem_kut = std.error(kut)) %>% 
+  summarise(mean_tv = mean(tv), sem_tv = std.error(tv)) %>% 
   ggplot() +
-  aes(x = job_type, y = mean_kut, colour = gender, ymin = mean_kut-sem_kut, ymax = mean_kut+sem_kut) +
+  aes(x = job_type, y = mean_tv, colour = gender, ymin = mean_tv-sem_tv, ymax = mean_tv+sem_tv) +
   geom_errorbar(width = 0.2, colour = rwthcolor$black) +
   geom_point(size = 3) +
   geom_line(aes(group = gender)) +
   scale_colour_manual(values = c(rwthcolor$blue, rwthcolor$red)) +
   theme_grey() +
-  labs(title = "Männer glauben eher daran, Technik kontrollieren zu können, Schüler und Studierende auch!", 
-       subtitle = "Punktdiagramm: KUT im Vergleich zwischen ArbeitnehmerInnen und Studierenden, gruppiert nach Geschlecht", 
-       x = "Beruf",
-       y = "KUT [1 - 6]",
+  labs(title = "Berufstätige Männer glauben eher, Technik zu verstehen als männliche Renter.", 
+       subtitle = "Punktdiagramm: TV im Vergleich zwischen RentnerInnen und ArbeitnehmerInnen, gruppiert nach Geschlecht", 
+       x = "Erwerbstätigkeit",
+       y = "TV [1 - 6]",
        colour = "Geschlecht",
-       caption = "Fehlerbalken zeigen Standardfehler des Mittelwertes") +
+       caption = "Fehlerbalken zeigen Standardfehler des Mittelwertes an.") +
   NULL
-#ggsave("HHerboltPaintnerPunktdiagrammANOVA.pdf", width = 6, height = 5)
 
+#ggsave("HerboltPaintnerPunktdiagrammANOVA.pdf", width = 6, height = 5)
