beccc479999800f54ad4da03d90eb86d0741c7c5
anneiou <anne.vollema@rwth-aachen.de>
2018-12-13 20:03:02 +0100

    Staging PDF

---
+++ calerovaldezhildebrandt.R
@@ -0,0 +1,21 @@
+# Bibliotheken laden
+
+library(tidyverse)
+library(psych)
+
+# Liste alle Verfügbaren Datensätze
+data()
+
+# Hilfe zu einem Datensatz (hier Titanic aus datasets)
+?starwars
+
+data(starwars)
+starwars
+
+
+# starwars, faithful, diamonds, mpg, msleep, Garcia, Tal_Or, affect, epi, galton, msq, iqitems, sai, spi
+data(gss_cat)
+gss_cat
+
+
+# Other datasets from other packages: http://www.public.iastate.edu/~hofmann/data_in_r_sortable.html
