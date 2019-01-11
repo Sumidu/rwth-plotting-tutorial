75db355d2901353ae13706158f47ccd99b1e2153
anneiou <anne.vollema@rwth-aachen.de>
2019-01-11 11:36:54 +0100

    Code (anne)

--- PetrikowksiVollema_likert.R
+++ PetrikowksiVollema_likert.R
@@ -3,4 +3,24 @@ library(likert)
 
 data <- read_rds("data/robo_pflege.rds")
 
-zustimmungsskala <- c("stimme gar nicht zu", "stimme nicht zu", "stimme eher nicht zu", )
+zustimmungsskala <- c("stimme gar nicht zu", "stimme nicht zu",
+                      "stimme eher nicht zu", "stimme eher zu",
+                      "stimme zu", "stimme völlig zu")
+
+data$robo_bath <- factor(data$robo_bath, labels = zustimmungsskala)
+data$robo_bed <- factor(data$robo_bed, labels = zustimmungsskala)
+
+data %>% select(robo_bath) %>% likert()
+
+
+
+
+
+
+
+
+
+
+pl
+
+ggsave(plot = pl, filename = "PetrikowskiVollema_likert.pdf", width=7, height = 4)
